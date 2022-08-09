94823720763bf4ed3d678ead0d26530dd8092335
Alexandr Larkin <larkin.alexandr@gmail.com>
2022-08-10 00:18:57 +0700

    Добавляет стили footer mobile, добавляет скрипт аккордион

--- source/js/main.js
+++ source/js/main.js
@@ -5,20 +5,54 @@ import {initModals} from './modules/modals/init-modals';
 
 window.addEventListener('DOMContentLoaded', () => {
 
-  // Utils
-  // ---------------------------------
+  // Accordion footer mobile
+  const footerAccordion = document.querySelectorAll('.footer__accordion');
+  const closeAll = () => {
+    for (let i = 0; i < footerAccordion.length; i++) {
+      footerAccordion[i].classList.remove('is-opened');
+      footerAccordion[i].classList.add('is-closed');
+    }
 
-  iosVhFix();
+    const openAccordion = (element) => {
+      element.classList.remove('is-closed');
+      element.classList.add('is-opened');
+    };
 
-  // Modules
-  // ---------------------------------
+    if (footerAccordion.length > 0) {
+      for (let i = 0; i < footerAccordion.length; i++) {
+        footerAccordion[i].classList.remove('no-js');
+        footerAccordion[i].classList.add('is-closed');
+        footerAccordion[i].addEventListener('click', (evt) => {
 
-  // все скрипты должны быть в обработчике 'DOMContentLoaded', но не все в 'load'
-  // в load следует добавить скрипты, не участвующие в работе первого экрана
-  window.addEventListener('load', () => {
-    initModals();
-  });
-});
+          if (evt.target.tagName === 'H3') {
+            if (evt.target.parentNode.classList.contains('is-closed')) {
+              closeAll();
+              openAccordion(evt.target.parentNode);
+
+            } else {
+              closeAll();
+            }
+          }
+        });
+      }
+    }
+    // ---------------------------------
+    // Utils
+    // ---------------------------------
+
+    iosVhFix();
+
+    // Modules
+    // ---------------------------------
+
+    // все скрипты должны быть в обработчике 'DOMContentLoaded', но не все в 'load'
+    // в load следует добавить скрипты, не участвующие в работе первого экрана
+    window.addEventListener('load', () => {
+      initModals();
+    });
+  };
+)
+  ;
 
 // ---------------------------------
 
