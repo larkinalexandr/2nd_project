dcf6bed1dbd1cabeab3420ed97ff01d0560e07b4
Alexandr Larkin <larkin.alexandr@gmail.com>
2022-08-15 14:44:54 +0700

    Добавляет модальное окно

--- source/js/main.js
+++ source/js/main.js
@@ -147,11 +147,12 @@ window.addEventListener('DOMContentLoaded', () => {
 
   // все скрипты должны быть в обработчике 'DOMContentLoaded', но не все в 'load'
   // в load следует добавить скрипты, не участвующие в работе первого экрана
-  window.addEventListener('load', () => {
-    initModals();
-  });
+
 });
 
+window.addEventListener('load', () => {
+  initModals();
+});
 // ---------------------------------
 
 // ❗❗❗ обязательно установите плагины eslint, stylelint, editorconfig в редактор кода.
